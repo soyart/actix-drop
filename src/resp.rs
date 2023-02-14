db5cbbce79380d486bd08417a0ec443887eb5191
Prem Phansuriyanon <prem@arm64.local>
2023-02-14 19:18:05 +0700

    feat: prepare to add json and plain HTTP response (cont)

--- src/resp.rs
+++ src/resp.rs
@@ -1,25 +1,42 @@
 use actix_web::{HttpResponse, HttpResponseBuilder};
+use serde_json::json;
 
 use crate::store::error::StoreError;
 
 type DropResult = Result<crate::Clipboard, StoreError>;
 
-trait DropResponse {
-    fn send_clipboard(self, builder: HttpResponseBuilder) -> HttpResponse;
+trait DropResponseHttp {
+    const CONTENT_TYPE: &'static str;
+    fn format_err(hash: &str, err: StoreError) -> String;
+    fn send_clipboard(self, hash: &str, builder: HttpResponseBuilder) -> HttpResponse;
+    fn post_clipboard(self, hash: &str, builder: HttpResponseBuilder) -> HttpResponse;
+}
+
+fn extract_error_msg(err: StoreError) -> String {
+    if let Some(public_err) = crate::error::public_error(err) {
+        return public_err.to_string();
+    }
+
+    return "private error".to_string();
 }
 
 struct ResponseHtml(DropResult);
-impl DropResponse for ResponseHtml {
-    fn send_clipboard(self, mut builder: HttpResponseBuilder) -> HttpResponse {
-        builder.content_type("text/html");
+impl DropResponseHttp for ResponseHtml {
+    const CONTENT_TYPE: &'static str = "text/html";
+
+    fn format_err(hash: &str, err: StoreError) -> String {
+        format!(
+            "<p>Error for clipboard {hash}: {}</p>",
+            extract_error_msg(err)
+        )
+    }
+
+    fn send_clipboard(self, hash: &str, mut builder: HttpResponseBuilder) -> HttpResponse {
+        builder.content_type(Self::CONTENT_TYPE);
 
         if let Err(err) = self.0 {
-            if let Some(public_err) = crate::error::public_error(err) {
-                let body = wrap_html(&format!("<p>error: {}</p>", public_err.to_string()));
-                return builder.body(body);
-            }
-
-            return builder.body(format!("private error"));
+            let body = wrap_html(&Self::format_err(hash, err));
+            return builder.body(body);
         }
 
         let maybe_body = String::from_utf8(self.0.unwrap().to_vec());
@@ -28,23 +45,43 @@ impl DropResponse for ResponseHtml {
             return builder.body(body);
         }
 
-        let body = wrap_html(&maybe_body.unwrap());
-        builder.body(body)
+        builder.body(wrap_html(&maybe_body.unwrap()))
+    }
+
+    fn post_clipboard(self, hash: &str, mut builder: HttpResponseBuilder) -> HttpResponse {
+        let body;
+
+        if let Err(err) = self.0 {
+            body = format!(
+                "<p>Error saving clipboard {hash}: {}</p>",
+                extract_error_msg(err)
+            );
+        } else {
+            body = format!(
+                r#"<p>Clipboard with hash <code>{hash}</code> created</p>
+        <p>The clipboard is now available at path <a href="/drop/{hash}/"><code>/drop/{hash}/</code></a></p>"#
+            );
+        }
+
+        builder
+            .content_type(Self::CONTENT_TYPE)
+            .body(wrap_html(&body))
     }
 }
 
 struct ResponsePlain(DropResult);
-impl DropResponse for ResponsePlain {
-    fn send_clipboard(self, mut builder: HttpResponseBuilder) -> HttpResponse {
-        builder.content_type("text/plain");
+impl DropResponseHttp for ResponsePlain {
+    const CONTENT_TYPE: &'static str = "text/plain; charset=utf-8";
+
+    fn format_err(hash: &str, err: StoreError) -> String {
+        format!("error for clipboard {hash}: {}", extract_error_msg(err))
+    }
+
+    fn send_clipboard(self, hash: &str, mut builder: HttpResponseBuilder) -> HttpResponse {
+        builder.content_type(Self::CONTENT_TYPE);
 
         if let Err(err) = self.0 {
-            if let Some(public_err) = crate::error::public_error(err) {
-                let body = format!("error: {}", public_err.to_string());
-                return builder.body(body);
-            }
-
-            return builder.body(format!("private error"));
+            return builder.body(Self::format_err(hash, err));
         }
 
         let maybe_body = String::from_utf8(self.0.unwrap().to_vec());
@@ -56,20 +93,37 @@ impl DropResponse for ResponsePlain {
         let body = maybe_body.unwrap();
         builder.body(body)
     }
+
+    fn post_clipboard(self, hash: &str, mut builder: HttpResponseBuilder) -> HttpResponse {
+        let body;
+
+        if let Err(err) = self.0 {
+            body = Self::format_err(hash, err);
+        } else {
+            body = format!("clipboard {hash} created and available at /api/drop/{hash}");
+        }
+
+        builder.content_type(Self::CONTENT_TYPE).body(body)
+    }
 }
 
 struct ResponseJson(DropResult);
-impl DropResponse for ResponseJson {
-    fn send_clipboard(self, mut builder: HttpResponseBuilder) -> HttpResponse {
-        builder.content_type("application/json");
+impl DropResponseHttp for ResponseJson {
+    const CONTENT_TYPE: &'static str = "application/json";
+
+    fn format_err(hash: &str, err: StoreError) -> String {
+        json!({
+            "error": extract_error_msg(err),
+            "clipboard": hash,
+        })
+        .to_string()
+    }
+
+    fn send_clipboard(self, hash: &str, mut builder: HttpResponseBuilder) -> HttpResponse {
+        builder.content_type(Self::CONTENT_TYPE);
 
         if let Err(err) = self.0 {
-            if let Some(public_err) = crate::error::public_error(err) {
-                let body = serde_json::to_string(&public_err).unwrap();
-                return builder.body(body);
-            }
-
-            return builder.body(format!("private error"));
+            return builder.body(Self::format_err(hash, err));
         }
 
         let maybe_body = String::from_utf8(self.0.unwrap().to_vec());
@@ -80,6 +134,21 @@ impl DropResponse for ResponseJson {
 
         let body = maybe_body.unwrap();
         builder.body(body)
+    }
+
+    fn post_clipboard(self, hash: &str, mut builder: HttpResponseBuilder) -> HttpResponse {
+        let body;
+        if let Err(err) = self.0 {
+            body = Self::format_err(hash, err);
+        } else {
+            body = json!({
+                "success": true,
+                "clipboard": hash,
+            })
+            .to_string();
+        }
+
+        builder.content_type(Self::CONTENT_TYPE).body(body)
     }
 }
 
