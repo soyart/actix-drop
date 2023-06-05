b61b8e630969bd0f4e8745713dc43c302ab3543f
artworkk <prem@cleverse.com>
2023-06-05 22:08:48 +0700

    chore: github username

--- src/resp/html.rs
+++ src/resp/html.rs
@@ -1,5 +1,5 @@
 const HEADER: &str = r#"<!DOCTYPE html><html><head><meta name=viewport content="width=device-width, initial-scale=1.0"><meta name=keywords content="actix-drop"><meta name=author content=@artnoi><meta charset=UTF-8><link href=https://artnoi.com/style.css rel=stylesheet><title>actix-drop</title></head><body><h1><a href="/">actix-drop</a></h1>"#;
-const FOOTER: &str = r#"<footer><p><a href="https://github.com/artnoi43/actix-drop">Contribute on Github</a></p></footer></body></html>"#;
+const FOOTER: &str = r#"<footer><p><a href="https://github.com/soyart/actix-drop">Contribute on Github</a></p></footer></body></html>"#;
 
 #[macro_export]
 macro_rules! tag_html {
